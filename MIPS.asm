2e2adadb6ce4409b1ae29ebd45bceb84e4ed66e9
Taviswhite <Taviswhite0907@gmail.com>
2025-12-02 16:33:26 -0500

    Update MIPS.asm

--- MIPS.asm
+++ MIPS.asm
@@ -12,3 +12,9 @@ main:
     li $v0, 4                   # system call for print string
     la $a0, prompt_msg          # load address of prompt message
     syscall                     # print prompt
+
+input_loop:
+    # Read integer from user
+    li $v0, 5                   # system call for read integer
+    syscall                     # read integer into $v0
+    move $t0, $v0               # store input in $t0 (N)
